611eee64ab10baa9bebc62b97faab0bbcc78824d
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2020-04-20 17:03:11 +1000

    Add files via upload

--- setupexe.py
+++ setupexe.py
@@ -4,16 +4,8 @@ import os, platform
 
 if platform.system() == 'Windows':
 	commandmv = 'move'
-	commandrm = 'rd /s /q'
 else:
 	commandmv = 'mv'
-	commandrm = 'rm -r'
 
 os.system('pyinstaller -y -F -w "MATE"')
-os.system('cd dist')
-if platform.system() != 'Windows':
-	os.system(commandmv + ' MATE MATE.exe')
-os.system(commandmv + ' MATE.exe ..')
-os.system('cd  ..')
-os.system(commandrm + ' build')
-os.system(commandrm + ' dist')
+os.system(commandmv + ' ' + os.path.join(os.getcwd(),'dist','MATE.exe') + ' .')
