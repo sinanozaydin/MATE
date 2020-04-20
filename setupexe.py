9cc1c03b4206447ec88142c7dcb8ebaa9fa175e8
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2020-04-20 17:16:30 +1000

    Update setupexe.py

--- setupexe.py
+++ setupexe.py
@@ -2,10 +2,13 @@
 
 import os, platform
 
+programpath = 'MATE.exe'
 if platform.system() == 'Windows':
 	commandmv = 'move'
+	programname = 'MATE.exe'
 else:
 	commandmv = 'mv'
+	programname = 'MATE'
 
 os.system('pyinstaller -y -F -w "MATE"')
-os.system(commandmv + ' ' + os.path.join(os.getcwd(),'dist','MATE.exe') + ' .')
+os.system(commandmv + ' ' + os.path.join(os.getcwd(),'dist',programname) + ' ' + os.path.join(os.getcwd(),programpath))
