f8028add7fd68d03e51dc16d55438eaeeba232b8
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2022-05-28 15:41:43 +1000

    upgraded solver

--- mate_src/cond_models/ol_wet_odd.py
+++ mate_src/cond_models/ol_wet_odd.py
@@ -19,11 +19,13 @@ def Dai2014a(model_method,T,P,corr_factor,fo2,fo2_ref,wt_err,ol_h2o):
 	q_dry = 0.16666 #Taken from Constable (2006)
 	wt_err = wt_err
 	h2o = ol_h2o / (1e4 * corr_factor)
+	#Changing fo2 to complex to avoid Runtime errors
+	fo2 = np.array(fo2, dtype = np.complex)
+	fo2_ref = np.array(fo2_ref, dtype = np.complex)
 	#p*dv multiplied by 1e3 to get them all on the basis of the SI units J/mol, which activation energy is in.
 	dai_ref_max = ((10.0**0.48) * np.exp(-((e1_dai - e1_dai_err) + (p_ref*dv_dai2014)) / (R_const * T))) + (10.0**2.84 * np.exp(-((e2_dai-e2_dai_err) + (p_ref*dv_dai2014)) / (R_const * T)))
 	dai_ref_min = ((10.0**0.48) * np.exp(-((e1_dai + e1_dai_err) + (p_ref*dv_dai2014)) / (R_const * T))) + (10.0**2.84 * np.exp(-((e2_dai+e2_dai_err) + (p_ref*dv_dai2014)) / (R_const * T)))
 	dai_ref = ((10.0**0.48) * np.exp(-((e1_dai) + (p_ref*dv_dai2014)) / (R_const * T))) + (10.0**2.84 * np.exp(-((e2_dai) + (p_ref*dv_dai2014)) / (R_const * T)))
-
 	cond_max_wet = (dai_ref_max * (((h2o + (h2o * wt_err)) / (cw_ref))**(r_dai-r_dai_err)) * (fo2/fo2_ref)**(q_dai)) *  np.exp(- ((P - p_ref) * dv_dai2014) / (R_const*T))
 	cond_min_wet = (dai_ref_min * (((h2o - (h2o * wt_err)) / (cw_ref))**(r_dai+r_dai_err)) * (fo2/fo2_ref)**(q_dai)) *  np.exp(- ((P - p_ref) * dv_dai2014) / (R_const*T))
 	cond_wet = (dai_ref * (h2o / (cw_ref))**(r_dai) * (fo2/fo2_ref)**(q_dai)) *  np.exp(- ((P - p_ref) * dv_dai2014) / (R_const*T))
@@ -31,15 +33,15 @@ def Dai2014a(model_method,T,P,corr_factor,fo2,fo2_ref,wt_err,ol_h2o):
 
 	if model_method == 0:
 
-		cond_max = dai_dry + cond_max_wet
-		cond_min = dai_dry + cond_min_wet
-		cond = dai_dry + cond_wet
+		cond_max = np.real(dai_dry + cond_max_wet)
+		cond_min = np.real(dai_dry + cond_min_wet)
+		cond = np.real(dai_dry + cond_wet)
 
 	elif model_method == 1:
 
-		cond_max = cond_max_wet
-		cond_min = cond_min_wet
-		cond = cond_wet
+		cond_max = np.real(cond_max_wet)
+		cond_min = np.real(cond_min_wet)
+		cond = np.real(cond_wet)
 
 	return cond_max,cond_min,cond,dai_dry,cond_wet
 
