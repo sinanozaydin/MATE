541eab2f858d0cb5398e0dc0e33fc13a6c6ced83
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2022-06-16 11:39:38 +1000

    added new melt models

--- mate_src/cond_models/melt_odd.py
+++ mate_src/cond_models/melt_odd.py
@@ -4,7 +4,7 @@ import numpy as np
 
 R_const = 8.3144621
 
-def Sifre2014(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+def Sifre2014_H2O_CO2(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
 
 	Melt_H2O = Melt_H2O * 1e-4 #converting ppm to wt percent
 	Melt_CO2 = Melt_CO2 * 1e-4 #converting ppm to wt percent
@@ -104,5 +104,100 @@ def Gaillard2005_K2O_Dependent(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
 	b = 4.742 - (0.6 * Melt_K2O)
 
 	cond = 10.0**(a + (b * (1.0/T)))
+
+	return cond, cond, cond
+
+def Guo2017_WetAndesite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+
+	cond = 10**(5.23 - (0.56 * (Melt_H2O**0.6)) - ((8130.4 - (1462.7*(Melt_H2O**0.6)) + ((581.3 - (12.7*Melt_H2O**2)) * P)) / T))
+
+	return cond, cond, cond
+
+def Laumonier2017_WetAndesite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+	P = P * 1e5 #converting gpa to bars
+
+	a = -0.34
+	b = 8.96
+	c = -8.07e-6
+	d = 1.67e-4
+	e = -9627
+	f = 1.25e5
+	g = -1.46e-1
+	h = 2.462
+
+	sigma_0 = np.exp(((a*Melt_H2O) + b) + (P * ((c*Melt_H2O) + d)))
+	Ea = (e * Melt_H2O) + f
+	dV = (g * Melt_H2O) + h
+
+	cond = sigma_0 * np.exp(-(Ea + (P * dV)) / (R_const * T))
+
+	return cond, cond, cond
+
+def Laumonier2015_WetDacite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+	P = P * 1e5 #converting gpa to bars
+
+	a = -0.064
+	b = 5.96
+	c = 1.06e-5
+	d = 2.49e-5
+	e = -6146
+	f = 88440
+	g = 0.176
+	h = 0.388
+
+	sigma_0 = np.exp(((a*Melt_H2O) + b) + (P * ((c*Melt_H2O) + d)))
+	Ea = (e * Melt_H2O) + f
+	dV = (g * Melt_H2O) + h
+
+	cond = sigma_0 * np.exp(-(Ea + (P * dV)) / (R_const * T))
+
+	return cond, cond, cond
+
+def Gaillard2004_WetRhyolite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+	P = P * 1e3
+
+
+	Ea = (-2925 * np.log(Melt_H2O)) + 64132.0
+	sigma_0 = (-78.9 * np.log(Melt_H2O)) + 754.0
+
+	cond = sigma_0 * np.exp((-Ea + (20*P)) / (R_const * T))
+
+	return cond, cond, cond
+
+def Guo2016_WetRhyolite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+
+	cond = 10**(2.983 - (0.0732*Melt_H2O) -\
+		((3528 - (233.8*Melt_H2O) + ((763 - 7.5*Melt_H2O**2)*P)) / T))
+
+	return cond, cond, cond
+
+def Chen2018_WetGranite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+
+	sigma_0 = 6.673 - (0.491*P)
+	Ea = 58987.0 - (2200 * np.log(Melt_H2O + 0.046))
+	dV = 10927.0 - (1981 * Melt_H2O)
+
+	cond = sigma_0 * np.exp(-(Ea + (P * dV)) / (R_const * T))
+
+	return cond, cond, cond
+
+def Guo2018_WetGranite(T, P, Melt_H2O, Melt_CO2, Melt_Na2O, Melt_K2O):
+
+	Melt_H2O = Melt_H2O * 1e-4 #converting to wt percent
+
+	cond = 10.0**(3.205 - (0.102 * Melt_H2O) -\
+		(-(4228.5 - (354.7 * Melt_H2O) + (693.6 * P)) / T))
 
 	return cond, cond, cond
