9e98509a74840eed3397f58d1eb982ecff6df6f7
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2022-06-21 13:49:48 +1000

    fixed co2 on write data and added novella2014 models of melt partition

--- mate_src/solidus_models/solidus.py
+++ mate_src/solidus_models/solidus.py
@@ -18,3 +18,13 @@ def Hirschmann2009_Dry(T,P,H2O,Melt_H2O):
     T_solidus = T_solidus + 273.15 #Converting to Kelvin
 
     return T_solidus
+
+def Katz2003(T, P, H2O, Melt_H2O):
+
+    T_solidus = 1085.7 + (132.9*P) - (5.1*P**2)
+    T_liquids_lherz = 1475.0 + (80.0*P) - (3.2*P**2)
+    T_liquidus = 1780.0 + (45.0*P) - (2.0*P**2)
+
+    R_cpx = 0.5 * (0.08 * P)
+
+    X_h2o_sat_melt = (12.0*P**0.6) + (1.0*P)
