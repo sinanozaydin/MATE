3c39b38076808935483d20b4486d18e084428ca0
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2022-06-25 11:07:58 +1000

    solidus stuff

---
+++ mate_src/solidus_models/carbon_solidus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+R_const = 8.3144621
+
+def Dasgupta2013_CarbonatedPeridotite(T, T_solidus, P, CO2_Melt):
+
+    CO2_Melt = CO2_Melt * 1e-4 #Converting to weight percent
+
+    delta_T = np.zeros(len(T))
+
+    for i in range(0,len(P)):
+
+        if P[i] <= 2.5:
+            a = 19.21
+            b = 1491.36
+            c = 0.86
+        elif (P[i] > 2.5) and (P[i] <= 3.5):
+            a = 27.04
+            b = 1490.75
+            c = 1.18
+        elif (P[i] > 3.5) and (P[i] <= 4.5):
+            a = 31.90
+            b = 1469.92
+            c = 1.31
+        elif (P[i] > 4.5):
+            a = -5.01
+            b = 1514.84
+            c = -1.23
+
+        delta_T[i] = (a * CO2_Melt[i]) + (b * np.log((1e2 - (c * CO2_Melt[i])) / 1e2))
+
+    T_solidus = T_solidus - delta_T
+
+    return T_solidus
--- mate_src/solidus_models/wet_solidus.py
+++ mate_src/solidus_models/wet_solidus.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
 import numpy as np
+
+R_const = 8.3144621
+
+def Hirschmann2009_WetPeridotite(T, T_solidus, P, Melt_Mass_Frac, H2O, D_per_melt, cpx_frac):
+
+    H2O = H2O / 1e4
+
+    Melt_H2O = H2O / (Melt_Mass_Frac + ((1.0 - Melt_Mass_Frac) * D_per_melt))
+
+    M = 59.0
+    dS = 0.4
+
+    X_h2o_melt = (2 * M * (Melt_H2O / 1e2)) /\
+        (18.02 + ((Melt_H2O / 1e2) * ((2 * M) - 18.02)))
+
+    T_solidus = T_solidus / (1 - ((R_const / M * dS) * np.log(1 - X_h2o_melt)))
+
+    return T_solidus
