c1298a83d4e605d7ccd89cfff21046725be18e5c
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2022-11-23 14:00:08 +1100

    dry solidus changes

--- mate_src/solidus_models/solidus.py
+++ mate_src/solidus_models/solidus.py
@@ -11,11 +11,18 @@ def Hirschmann2009_DryPeridotite(T, P):
     for i in range(0,len(P)):
 
         if P[i] <= 10.0:
-            T_solidus[i] = (-5.1404654*(P[i]**2.0)) +\
-                (132.899012 * P[i]) + 1124.123
+            T_solidus[i] = (-1.092 * (P[i] - 10.0)**2.0) +\
+                (32.39 * (P[i] - 10.0)) + 1935.0
         elif P[i] > 10.0:
-            T_solidus[i] = (-1.092*(P[i]-10)**2.0) +\
-                (32.39 * (P[i]-10)) + 1395.0
+            T_solidus[i] = (26.53 * (P[i]-23.5)) + 2175.0
+
+    T_solidus = T_solidus + 273.15 #Converting to Kelvin
+
+    return T_solidus
+
+def Hirschmann2000_DryPeridotite(T, P):
+
+    T_solidus = (-5.104 * (P**2.0)) + (132.899 * P) + 1120.661
 
     T_solidus = T_solidus + 273.15 #Converting to Kelvin
 
