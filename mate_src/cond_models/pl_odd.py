050c48adb37b2f7b1e922d2e7facc54ba3ca44fb
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2023-08-16 13:18:46 +1000

    Update pl_odd.py

--- mate_src/cond_models/pl_odd.py
+++ mate_src/cond_models/pl_odd.py
@@ -4,6 +4,15 @@ import numpy as np
 
 R_const = 8.3144621
 
+def Li2016_Phlogopite(T, P, param1, param2, method):
+
+	A_li_001 = 10**10.15
+	A_li_010 = 10**8.41#!/usr/bin/env python3
+
+import numpy as np
+
+R_const = 8.3144621
+
 def Li2016(T,flu,method):
 
 	A_li_001 = 10**10.15
@@ -61,3 +70,27 @@ def Li2017(T,flu,method):
 		cond_min = cond
 
 	return cond_max,cond_min,cond
+
+	A_li_110 = 10**6.09
+
+	E_li_001 = 204000.0
+	E_li_010 = 179000.0
+	E_li_110 = 134000.0
+
+	cond_001 = A_li_001 * np.exp(-(E_li_001) / (R_const * T))
+	cond_010 = A_li_010 * np.exp(-(E_li_010) / (R_const * T))
+	cond_110 = A_li_110 * np.exp(-(E_li_110) / (R_const * T))
+
+	cond = (cond_001 * cond_010 * cond_110)**(1.0/3.0)
+
+	return cond
+
+def Li2017_Phlogopite_param1_F(T, P, param1, param2, method):
+
+	A_li = 10**8.59
+	E_li = 191000.0
+	r_li = 0.98
+
+	cond = A_li * (param1**r_li) * np.exp(-E_li / (R_const * T))
+
+	return cond
